12a49e3734c528cc90d5e30aa861102b33e77223
Gökçe Uludoğan <gokceuludogan@gmail.com>
2026-05-08 15:23:06 +0300

    Delete build directory

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_2048_FILE /Users/curiousspeck/qt-minesweeper/assignment3-spring26/build/2048)
-set(__QT_DEPLOY_TARGET_2048_TYPE EXECUTABLE)
--- build/CMakeFiles/2048.dir/2048_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/2048.dir/2048_autogen/mocs_compilation.cpp.o: \
-  /Users/curiousspeck/qt-minesweeper/assignment3-spring26/build/2048_autogen/mocs_compilation.cpp
